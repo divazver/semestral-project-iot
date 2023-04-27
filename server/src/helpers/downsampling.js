35ad2ad76bc63f907a35e30cbdab4efa91eb4cdf
Antonín Martykán <a.martykan@gmail.com>
2023-04-28 01:16:54 +0200

    down-/upsamplling updated, changed into classes

--- server/src/helpers/downsampling.js
+++ server/src/helpers/downsampling.js
@@ -1,61 +1,129 @@
-/*
- * function to calculate downsampled data from the original data
- * @param {Array} data - The list of n (n is arbitrary large) temperature & humidity values passed as an Array of Objects [{temperature: number, humidity: number}, ...]
- * @returns {Array} downsampledData - The list of k temperature & humidity values returned as an Array of Objects [{temperature: number, humidity: number}, ...]
- * where k < n
- */
+/*¨
+* 1) vytvořit objekt Downsampling nad dateFrom - dateTo z FE, pokud granularita > 1 min
+* 2) zavolat metodu downsamplingTimeTransformation --> return value od - do Date objects na dotaz v DB
+* 3) dotaz v DB --> return value  interval x hodnot
+* 4) zavolat metodu downsamping s daty z DB
+* 5) poslat na FE
+*/
+export class Downsampling {
+    // dateFrom, dateTo and granularity are needed
+    constructor(dateFrom, dateTo, granularity) {
+        this.dateFrom = new Date(dateFrom);
+        this.dateTo = new Date(dateTo);
+        // min to ms
+        this.granularity = granularity * 60 * 1000;
 
-export function downsampling(data, k) {
-    const n = data.length;
-    if (n < k) {
-        throw new Error("Can't downsample data to a larger dataset!")
+        // attribute used for downsampling function
+        this.datapoints = null;
+
+        // array of calculated datapoints, descriptions
+        this.datapointsArray = [];
+        this.descriptionsArray = [];
+
+        // merging of datapoints & descriptions - final output
+        this.completedWeatherData = [];
     }
-    // there will be k elements in the final Array k pivots must be calculated
-    const step = n / k;
-    // array of pivots to split the original Array into k intervals
-    const pivots = [];
-    // computing pivots of the dataset
-    for (let i = 1; i <= k; i++) {
-        pivots.push(Math.floor(i * step));
-    }
-    // extracting temperatures and humidities from the objects
-    const temperatures = data.map(obj => obj.temperature);
-    const humidities = data.map(obj => obj.humidity);
 
-    // array to store the means of temperature and humidity in
-    const meansTemperature = [];
-    const meansHumidity = [];
 
-    // temporary values for the loop
-    let sumTemperatures = 0;
-    let sumHumidities = 0;
-    let count = 0;
-    
-    // loop iterating through all n values, computing means of deciles
-    for (let i = 0; i < n; i++) {
-        // pivot = breakpoint --> new mean will be calculated
-        if (pivots.includes(i + 1)) {
-            // means calculated + pushed to the arrays
-            meansTemperature.push(sumTemperatures / count);
-            meansHumidity.push(sumHumidities / count);
-            // temporary values have to be updated to the current iterated value, not to zero!
-            sumTemperatures = temperatures[i];
-            sumHumidities = humidities[i];
-            count = 1;
+    /*
+    * method to calculate downsampled data from the original data
+    * @param {Array} data - The list of n (n is arbitrary large) temperature & humidity values passed as an Array of Objects [{temperature: number, humidity: number}, ...]
+    * @returns {Array} downsampledData - The list of k temperature & humidity values returned as an Array of Objects [{temperature: number, humidity: number}, ...]
+    * where k < n AND k = this.datapoints
+    */
+    downsampling(data) {
+        const n = data.length;
+        if (n < this.datapoints) {
+            throw new Error("Can't downsample data to a larger dataset!")
         }
-        // otherwise simple update of values
-        else {
-            sumTemperatures += temperatures[i];
-            sumHumidities += humidities[i];
-            count++;
+        // there will be k elements in the final Array k pivots must be calculated
+        const step = n / this.datapoints;
+        // array of pivots to split the original Array into k intervals
+        const pivots = [];
+        // computing pivots of the dataset
+        // starting with 2, because the first "pivot"- datapoint - will be the first DB entry
+        for (let i = 2; i <= this.datapoints; i++) {
+            pivots.push(Math.floor(i * step));
         }
+        // extracting temperatures and humidities from the objects
+        const temperatures = data.map(obj => obj.temperature);
+        const humidities = data.map(obj => obj.humidity);
+    
+        // array to store the means of temperature and humidity in- first element will be always the first datapoint
+        const meansTemperature = [data[0].temperature];
+        const meansHumidity = [data[0].humidity];
+    
+        // temporary values for the loop
+        let sumTemperatures = 0;
+        let sumHumidities = 0;
+        let count = 0;
+        
+        // loop iterating through all n values, computing means of deciles
+        for (let i = 0; i < n + 1; i++) {
+            // pivot = breakpoint --> new mean will be calculated
+            if (pivots.includes(i)) {
+                // means calculated + pushed to the arrays
+                meansTemperature.push(sumTemperatures / count);
+                meansHumidity.push(sumHumidities / count);
+                // temporary values have to be updated to the current iterated value, not to zero!
+                sumTemperatures = temperatures[i];
+                sumHumidities = humidities[i];
+                count = 1;
+            }
+            // otherwise simple update of values
+            else {
+                sumTemperatures += temperatures[i];
+                sumHumidities += humidities[i];
+                count++;
+            }
+        }
+        
+        // storing all data into an object, that will be returned
+        this.datapointsArray = meansTemperature.map((mean, index) => ({
+            temperature: mean,
+            humidity: meansHumidity[index]
+          }));
+
+        // creating compound objects
+        for (let i = 0; i < this.datapointsArray.length; i++) {
+        let obj = {
+            temperature: this.datapointsArray[i].temperature,
+            humidity: this.datapointsArray[i].humidity,
+            description: new Date(this.descriptionsArray[i])
+        };
+        this.completedWeatherData.push(obj);
+        }
+        return this.completedWeatherData;
+    
+    }
+
+
+
+    // in case the granularity is set to >1minute, data must be downsampled
+    // beforehand the date from - to information has to be converted to the inputs of the downsampling function
+    // this is the method to do it
+    downsamplingTimeTransformation() {
+        let fiveMinutes = 5 * 60 * 1000;
+        // dateFrom must be ceiled, so that the graph starts with an existing datapoint
+        let dateFromCeiled = new Date(Math.ceil(this.dateFrom.getTime() / fiveMinutes) * fiveMinutes);
+        // calculating timestamps (so that it doesn't have to be done every iteration)
+        let dateFromCeiledTimestamp = dateFromCeiled.getTime();
+        let dateToTimestamp = this.dateTo.getTime();
+        let dateToFloored;
+        // we want to "floor" dateTo, but not to the closest 5-min interval - it has to take granularity into account
+        while (dateFromCeiledTimestamp < dateToTimestamp) {
+            // every iteration timestamp is pushed into descriptions
+            this.descriptionsArray.push(new Date(dateFromCeiledTimestamp));
+            // number of datapoints is increased
+            this.datapoints++;
+            // dateToFloored (final output) is updated
+            dateToFloored = dateFromCeiledTimestamp;
+            // chosen granularity is added
+            dateFromCeiledTimestamp += this.granularity;
+        }
+
+        return([dateFromCeiled, dateToFloored])
+
     }
     
-    // storing all data into an object, that will be returned
-    const downsampledData = meansTemperature.map((mean, index) => ({
-        temperature: mean,
-        humidity: meansHumidity[index]
-      }));
-      
-    return downsampledData;
 }
--- server/src/helpers/upsampling.js
+++ server/src/helpers/upsampling.js
@@ -1,94 +1,212 @@
-// function for solving LSE
-import {solveLinearSystem} from './solve-lse.js'
+/*¨
+* 1) vytvořit objekt Upsampling nad dateFrom - dateTo z FE, pokud granularita = 1 min
+* 2) zavolat metodu upsamplingTimeTransformation --> return value od - do Date objects na dotaz v DB
+* 3) dotaz v DB --> return value  interval osmi hodnot
+* 4) zavolat metodu upsamping s daty z DB
+* 5) poslat na FE
+*/
+export class Upsampling {
+    // only dateFrom and dateTo are needed - granularity is automaticaly 1-minute
+    constructor(dateFrom, dateTo) {
+        this.dateFrom = new Date(dateFrom);
+        this.dateTo = new Date(dateTo);
 
-/*
- * function to calculate upsampled data from downsampled data
- * @param {Array} downsampledData - The list of 8 temperature & 8 humidity values passed as an Array of Objects [{temperature: number, humidity: number}, ...]
- * covering the longest possible interval with 1 minute granularity - 30 min - starting at any index <0, 4>
- * 
- * | - - - - | - - - - | - - - - | - - - - | - - - - | - - - - | - - - - |
- * 0 min     5 min    10 min    15 min    20 min    25 min    30 min    35 min
- * 
- * 
- *                                                                                  "x" = datapoint input - every 5 minutes
- *                                                           /-x---------x          "-", "/", "\" - polynomial representation
- *      -----x---------x------                    /-x--------
- *     /                      \--x---------x------    
- * x--/
- * 
- * _______________________________________________________________________   x-Axe
- * 1         2         3         4         5         6         7         8
- * 
- * @param {int} index - "index" of the first minute, that should be calculated <1, 34> indexing starts with 1 (better for further calculations)
- * 
- * @param {int} datapoints - number of data points, that should be calculated starting with index, where index + datapoints <= 36 AND 2 <= datapoints <= 30
- * 
- * @returns {Array} upsampledData - The list of n temperature & humidity values passed as an Array of Objects [{temperature: number, humidity: number}, ...] covering the interval
- * 
- */
-export function upsampling(downsampledData, index, datapoints) {
-    // checking if the input data is valid (array of length 8)
-    if (!Array.isArray(downsampledData) || downsampledData.length !== 8) {
-        throw new Error('Invalid parameter "downsampledData: must be an Array of 8 values');
+        // attributes used for upsampling function
+        this.index = null;
+        this.datapoints = null;
+
+        // array of calculated datapoints, descriptions
+        this.datapointsArray = [];
+        this.descriptionsArray = [];
+
+        // merging of datapoints & descriptions - final output
+        this.completedWeatherData = [];
     }
-    // checking, if the number of datapoints is correct
-    if (datapoints < 2 || datapoints > 30) {
-        throw new Error('Invalid parameter "datapoints": must be an integer between 2 and 30')
-    }
-    // checking, if the index won't be outside of calculated interval
-    if (index + datapoints > 36) {
-        throw new Error('Invalid parameters: indexed values are out of range')
 
-    }  
 
-    // extracting two lists of data - humidity and temperature
-    const temperatures = downsampledData.map(obj => obj.temperature);
-    const humidities = downsampledData.map(obj => obj.humidity);
-
-    // creating two Arrays for two function calls (will be modified)
-    const A1 = []
-    const A2 = []
-
-    /* 
-    creating matrices for LSE
-    */
-    for (let i = 0; i < 8; i++) {
-        const row = [];
-        for (let j = 7; j >= 0; j--) {
-          row.push(Math.pow(i + 1, j));
+    // helper method to create interpolation matrix of n degree
+    createInterpolationMatrix(n) {
+        let matrix = []
+        for (let i = 0; i < n; i++) {
+            const row = [];
+            for (let j = n - 1; j >= 0; j--) {
+            row.push(Math.pow(i + 1, j));
+            }
+            matrix.push(row);
         }
-        A1.push(row);
-        // shallow copy
-        A2.push([...row])
-      }      
-
-    // using polynomial interpolation of 7th degree to get coeficients of the temperature/humidity curve
-    const [a, b, c, d, e, f, g, h] = solveLinearSystem(A1, temperatures);
-    const [i, j, k, l, m, n, o, p] = solveLinearSystem(A2, humidities);
-
-    // lists to store the upsampled values that will be further used
-    const upsampledTemperatures = [];
-    const upsampledHumidities = [];
-    
-    // 1 is the first datapoint on our curve (see graph)
-    // one step = one minute = 1/5 = 0.2
-    // step length * (number of steps - 1) --> first step is the datapoint x = 1
-    const lowerBound = 1 + 0.2 * (index - 1)
-    //lowerBound + the number of steps specified (1 subtracted - 1st datapoint already included) we need to take in order to get n values
-    // +0.1 due to rounding error
-    const upperBound = lowerBound + (datapoints - 1) * 0.2 + 0.1
-
-    // calculating upsampled data - x-coordinates (index, index + 0.2, index + 0.4 ... index + datapoints * 0.2) of the curve => datapoint values
-    for (let q = lowerBound; q <= upperBound; q += 0.2) {
-        upsampledTemperatures.push(a * q ** 7 + b * q ** 6 + c * q ** 5 + d * q ** 4 + e * q ** 3 + f * q ** 2 + g * q + h);
-        upsampledHumidities.push(i * q ** 7 + j * q ** 6 + k * q ** 5 + l * q ** 4 + m * q ** 3 + n * q ** 2 + o * q + p);
+        return matrix;
     }
 
-    // storing all data into an object, that will be returned
-    const upsampledData = upsampledTemperatures.map((temp, index) => ({
-        temperature: temp,
-        humidity: upsampledHumidities[index]
-    }));
+    // helper function to solve a LSE given matrix A and vector b
+    solveLinearSystem(A, b) {
+        // Check that the input is a valid system of linear equations
+        if (!Array.isArray(A) || !Array.isArray(b) || A.length !== b.length) {
+          throw new Error('Invalid input: A must be a matrix and b must be a vector!');
+        }
+      
+        const n = A.length;
+      
+        // Forward elimination
+        for (let i = 0; i < n; i++) {
+          // Find the row with the largest absolute value in the ith column
+          let maxRow = i;
+          for (let j = i + 1; j < n; j++) {
+            if (Math.abs(A[j][i]) > Math.abs(A[maxRow][i])) {
+              maxRow = j;
+            }
+          }
+          // Swap the ith row with the row with the largest absolute value in the ith column
+          [A[i], A[maxRow]] = [A[maxRow], A[i]];
+          [b[i], b[maxRow]] = [b[maxRow], b[i]];
+          // Eliminate the ith column in the remaining rows
+          for (let j = i + 1; j < n; j++) {
+            const factor = A[j][i] / A[i][i];
+            for (let k = i + 1; k < n; k++) {
+              A[j][k] -= factor * A[i][k];
+            }
+            b[j] -= factor * b[i];
+          }
+        }
+      
+        // Back-substitute to solve for the unknowns
+        const x = new Array(n).fill(0);
+        for (let i = n - 1; i >= 0; i--) {
+          let sum = 0;
+          for (let j = i + 1; j < n; j++) {
+            sum += A[i][j] * x[j];
+          }
+          x[i] = (b[i] - sum) / A[i][i];
+        }
+      
+        return x;
+      }
+      
 
-    return upsampledData;
+    /*
+    * method to calculate upsampled data (granularity 1 minute)
+    * @param {Array} downsampledData - The list of 8 temperature & 8 humidity values passed as an Array of Objects [{temperature: number, humidity: number}, ...]
+    * covering the longest possible interval with 1 minute granularity - 30 min - starting at any index <0, 4>
+    * 
+    * | - - - - | - - - - | - - - - | - - - - | - - - - | - - - - | - - - - |
+    * 0 min     5 min    10 min    15 min    20 min    25 min    30 min    35 min
+    * 
+    * 
+    *                                                                                  "x" = datapoint input - every 5 minutes
+    *                                                           /-x---------x          "-", "/", "\" - polynomial representation
+    *      -----x---------x------                    /-x--------
+    *     /                      \--x---------x------    
+    * x--/
+    * 
+    * _______________________________________________________________________   x-Axe
+    * 1         2         3         4         5         6         7         8
+    * 
+    * @param {int} index - "index" of the first minute, that should be calculated <1, 34> indexing starts with 1 (better for further calculations)
+    * 
+    * @param {int} datapoints - number of data points, that should be calculated starting with index, where index + datapoints <= 36 AND 2 <= datapoints <= 30
+    * 
+    * @returns {Array} upsampledData - The list of n temperature & humidity values passed as an Array of Objects [{temperature: number, humidity: number}, ...] covering the interval
+    * 
+    */
+    upsampling(downsampledData) {
+        // checking if the input data is valid (array of length 8)
+        if (!Array.isArray(downsampledData) || downsampledData.length !== 8) {
+            throw new Error('Invalid parameter "downsampledData: must be an Array of 8 values');
+        }
+        // checking, if the number of datapoints is correct
+        if (this.datapoints < 2 || this.datapoints > 30) {
+            throw new Error('Invalid parameter "datapoints": must be an integer between 2 and 30')
+        }
+        // checking, if the index won't be outside of calculated interval
+        if (this.index + this.datapoints > 37) {
+            throw new Error('Invalid parameters: indexed values are out of range')
+        }  
+
+        // extracting two lists of data - humidity and temperature
+        const temperatures = downsampledData.map(obj => obj.temperature);
+        const humidities = downsampledData.map(obj => obj.humidity);
+
+        // creating two Arrays for two function calls (will be modified)
+        const A1 = this.createInterpolationMatrix(8);
+        const A2 = this.createInterpolationMatrix(8);
+
+        // using polynomial interpolation of 7th degree to get coeficients of the temperature/humidity curve
+        const [a, b, c, d, e, f, g, h] = this.solveLinearSystem(A1, temperatures);
+        const [i, j, k, l, m, n, o, p] = this.solveLinearSystem(A2, humidities);
+
+        // lists to store the upsampled values that will be further used
+        const upsampledTemperatures = [];
+        const upsampledHumidities = [];
+        
+        // 1 is the first datapoint on our curve (see graph)
+        // one step = one minute = 1/5 = 0.2
+        // step length * (number of steps - 1) --> first step is the datapoint x = 1
+        const lowerBound = 1 + 0.2 * (this.index - 1)
+        //lowerBound + the number of steps specified (1 subtracted - 1st datapoint already included) we need to take in order to get n values
+        // +0.1 due to rounding error
+        const upperBound = lowerBound + (this.datapoints - 1) * 0.2 + 0.1
+
+        // calculating upsampled data - x-coordinates (index, index + 0.2, index + 0.4 ... index + datapoints * 0.2) of the curve => datapoint values
+        for (let q = lowerBound; q <= upperBound; q += 0.2) {
+            upsampledTemperatures.push(a * q ** 7 + b * q ** 6 + c * q ** 5 + d * q ** 4 + e * q ** 3 + f * q ** 2 + g * q + h);
+            upsampledHumidities.push(i * q ** 7 + j * q ** 6 + k * q ** 5 + l * q ** 4 + m * q ** 3 + n * q ** 2 + o * q + p);
+        }
+
+        // storing all data into datapoints array
+        this.datapointsArray = upsampledTemperatures.map((temp, index) => ({
+            temperature: temp,
+            humidity: upsampledHumidities[index]
+        }));
+
+        for (let i = 0; i < this.datapointsArray.length; i++) {
+            let obj = {
+              temperature: this.datapointsArray[i].temperature,
+              humidity: this.datapointsArray[i].humidity,
+              description: new Date(this.descriptionsArray[i])
+            };
+            this.completedWeatherData.push(obj);
+          }
+          
+
+        return this.completedWeatherData;
+
+
+    }
+
+    // in case the granularity is set to 1 minute, data must be upsampled
+    // beforehand the date from - to information has to be converted to the inputs of the upsampling function
+    // this is the method to do it
+    upsamplingTimeTransformation() {
+
+        // the interval has to be stretched to the nearest multiples of 5-minutes, so that the required time interval fits inside it
+        let fiveMinutes = 5 * 60 * 1000; // 5 minutes in milliseconds
+        let oneMinute = 60 * 1000;
+
+        // storing the number of datapoints that need to be retrieved from the DB
+        this.datapoints = 1 + (this.dateTo.getTime() - this.dateFrom.getTime()) / oneMinute;
+        console.log("datapoints", this.datapoints)
+        
+        let datapointTimestamp = this.dateFrom.getTime();
+        for (let i = 0; i < this.datapoints; i++){
+            this.descriptionsArray.push(new Date(datapointTimestamp + (i * oneMinute)))
+        }
+
+
+        let dateFromFloored = new Date(Math.floor(this.dateFrom.getTime() / fiveMinutes) * fiveMinutes);
+        let dateToCeiled = new Date(Math.ceil(this.dateTo.getTime() / fiveMinutes) * fiveMinutes);
+
+
+        // we need an interval of 8 five minute values(=7 minutes intervals) from the DB - see JSDocs for upsamplnig
+        let currentNumberOfIntervals = (dateToCeiled.getTime() - dateFromFloored.getTime()) / fiveMinutes;
+        
+        let dateFromForUpsampling = new Date(dateFromFloored.getTime() - ((7 - currentNumberOfIntervals) * fiveMinutes));
+
+        //storing the "index" of the first relevant timestamp for upsampling function
+        this.index = ((this.dateFrom.getTime() - dateFromForUpsampling.getTime()) / oneMinute) + 1
+        console.log("index: ", this.index)
+
+        // V TUTO CHVÍLI SE VRACÍ 2 DATA - OD/DO - Z DB JE POTŘEBA VYTÁHNOUT VŠECHNA DATA (po 5 minutách) LEŽÍCÍ MEZI TĚMITO INTERVALY (VČETNĚ) = CELKEM 8 HODNOT
+        return([dateFromForUpsampling, dateToCeiled])
+
+    }
+
 }
